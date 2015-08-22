0314d48f854b2503107a828e9dd429d51d9e1eab
vages <eirikvageskar@gmail.com>
2015-08-22 13:46:19 +0200

    Update so that three simultaneous searches are displayed

--- module_1/BoardSearchRunner.py
+++ module_1/BoardSearchRunner.py
@@ -7,22 +7,56 @@ Board = B.Board
 
 
 class BoardSearchRunner:
-    def __init__(self, board_specification):
+    def __init__(self, board_specification, search_modes):
         self.board_specification = board_specification
         self.board = Board(board_specification)
-        self.solution = None
+        self.search_modes = search_modes
+        self.results = dict()
 
-    def run_search(self, mode='best_first'):
-        self.solution = Bfs.a_star(self.board_specification[1], self.board_specification[2], self.board.get_neighbours,
-                                   Board.distance_between, Board.distance_between, mode=mode)
+    def run_search(self):
+        for mode in self.search_modes:
+            self.results[mode] = Bfs.a_star(self.board_specification[1],
+                                              self.board_specification[2],
+                                              self.board.get_neighbours,
+                                              Board.distance_between,
+                                              Board.distance_between,
+                                              mode=mode)
 
-    def draw_board_and_solution(self, width, height):
-        cell_width = int(min(height/self.board.y_size, width/self.board.x_size))
-        width, height = self.board.x_size*cell_width, self.board.y_size*cell_width
+    def draw_board_on_surface(self, surface, position_tuple, size_tuple, result):
+        x_origin, y_origin = position_tuple
+        width, height = size_tuple
         WHITE = (0xFF, 0xFF, 0xFF)
         RED = (0xFF, 0, 0)
         BLACK = (0, 0, 0)
 
+        cell_width = int(min(height/self.board.y_size, width/self.board.x_size))
+
+        # Draw the environment
+        for i in range(self.board.y_size):
+            for j in range(self.board.x_size):
+                if self.board.get_cell((j, i)) == 1:
+                    color = RED
+                else:
+                    color = WHITE
+                pygame.draw.rect(surface, color, [x_origin+j*cell_width, y_origin+i*cell_width, cell_width, cell_width])
+
+        # Draw dots in open spaces
+        for i in range(self.board.y_size):
+            for j in range(self.board.x_size):
+                if self.board.get_cell((j, i)) == 0:
+                    pygame.draw.circle(surface, BLACK, (int(x_origin+j*cell_width+cell_width/2), int(y_origin+i*cell_width+cell_width/2)), int(cell_width/16))
+
+        pointlist = []
+        for i in range(len(result)):
+            x, y = result[i]
+            pointlist.append((x_origin+x*cell_width+int(cell_width/2), y_origin+y*cell_width+int(cell_width/2)))
+
+        pygame.draw.lines(surface, RED, False, pointlist, int(cell_width/16))
+
+    def draw_board_and_solution(self, width, height):
+        cell_width = int(min(height/self.board.y_size, width/self.board.x_size))
+        width, height = self.board.x_size*cell_width, self.board.y_size*cell_width
+        size_of_one_board = (width/2, height/2)
         pygame.init()
         screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Search visualization")
@@ -41,34 +75,20 @@ class BoardSearchRunner:
                 if event.type == pygame.QUIT:  # If user clicked close
                     done = True  # Flag that we are done so we exit this loop
 
-            # Draw the environment
-            for i in range(self.board.y_size):
-                for j in range(self.board.x_size):
-                    if self.board.get_cell((j, i)) == 1:
-                        color = RED
-                    else:
-                        color = WHITE
-                    pygame.draw.rect(screen, color, [j*cell_width, i*cell_width, cell_width, cell_width])
-
-            # Draw dots in open spaces
-            for i in range(self.board.y_size):
-                for j in range(self.board.x_size):
-                    if self.board.get_cell((j, i)) == 0:
-                        pygame.draw.circle(screen, BLACK, (int(j*cell_width+cell_width/2), int(i*cell_width+cell_width/2)), int(cell_width/16))
-
-            pointlist = []
-            for i in range(len(self.solution)):
-                x, y = self.solution[i]
-                pointlist.append((x*cell_width+int(cell_width/2), y*cell_width+int(cell_width/2)))
-
-            pygame.draw.lines(screen, RED, False, pointlist, int(cell_width/16))
+            self.draw_board_on_surface(screen, (0, 0), size_of_one_board, self.results["best_first"])
+            self.draw_board_on_surface(screen, (width/2, 0), size_of_one_board, self.results["depth_first"])
+            self.draw_board_on_surface(screen, (0, height/2), size_of_one_board, self.results["breadth_first"])
+            pygame.draw.line(screen, (0, 0, 0), (0, height/2), (width, height/2), 2)
+            pygame.draw.line(screen, (0, 0, 0), (width/2, 0), (width/2, height), 2)
             pygame.display.flip()
 
-            clock.tick(30)
+            clock.tick(10)
 
 
 if __name__ == '__main__':
     f = open(sys.argv[1])
+
+    # Format input file
     spec_raw = f.readline().split(';')
     spec_final = []
     for i in range(len(spec_raw)):
@@ -80,10 +100,6 @@ if __name__ == '__main__':
 
     spec_final = tuple(spec_final)
 
-    bsr = BoardSearchRunner(spec_final)
-    try:
-        mode = sys.argv[2]
-    except IndexError:
-        mode = "best_first"
-    bsr.run_search(mode)
+    bsr = BoardSearchRunner(spec_final, ['best_first', 'depth_first', 'breadth_first'])
+    bsr.run_search()
     bsr.draw_board_and_solution(1280, 800)
