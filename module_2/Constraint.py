3bb4922cc15e267fd4e9276c12e169ecaae0bd4c
vages <eirikvageskar@gmail.com>
2015-09-29 11:32:35 +0200

    Add support for storing the actual variables involved in the constraint in Constraint.

--- module_2/Constraint.py
+++ module_2/Constraint.py
@@ -2,11 +2,18 @@ __author__ = 'eirikvageskar'
 
 
 class Constraint:
-    def __init__(self, var_names, expression):
-        args = var_names[0]
-        for i in range(1, len(var_names)):
-            args += ', ' + var_names[i]
+    def __init__(self, func_var_names, expression, actual_var_names=None):
+        if actual_var_names is None:
+            actual_var_names = func_var_names
+
+        args = func_var_names[0]
+
+        for i in range(1, len(func_var_names)):
+            args += ', ' + func_var_names[i]
+
         self.constraint_formula = eval('lambda ' + args + ': ' + expression)
+
+        self.variables = tuple(actual_var_names)
 
     def evaluate_constraint(self, args):
         parameters = str(args[0])
