3b287c2e742785ffef73e40ac066fcbcc0353ed5
vages <eirikvageskar@gmail.com>
2015-09-29 20:55:23 +0200

    Flesh out init-method. Class should be about finished.

--- module_3/NonogramProblem.py
+++ module_3/NonogramProblem.py
@@ -1,11 +1,33 @@
 from module_2.CSProblem import CSProblem
+from module_2.Constraint import Constraint
 
 CSP = CSProblem
 
 
 class NonogramProblem(CSP):
-    def __init__(self):
+    def __init__(self, spec):
         super(NonogramProblem, self).__init__()
+
+        self.x_size, self.y_size = spec[0]
+
+        for i in range(self.y_size):
+            row_name = 'y'+str(i)
+            row_spec = spec[self.y_size-i]
+            self.domains[row_name] = set(self.create_all_possible_combinations_from_line_spec(row_spec, self.x_size))
+
+        for i in range(self.x_size):
+            col_name = 'x'+str(i)
+            col_spec = spec[1+self.y_size+i]
+            self.domains[col_name] = set(self.create_all_possible_combinations_from_line_spec(col_spec, self.y_size))
+
+        for j in range(self.x_size):
+            for k in range(self.y_size):
+                line_expression = 'x[' + str(k) + '] == y[' + str(j) + ']'
+                line_constraint = Constraint(['x', 'y'], line_expression, ('x' + str(j), 'y' + str(k)))
+                self.constraints.append(line_constraint)
+
+        self.initialize_queue()
+        self.domain_filtering()
 
     @staticmethod
     def _fill_k_bits_in_array_from_position_i(array, k, i):
