0341474e6afdf495034fe18631df9ee61cd7e4c9
vages <eirikvageskar@gmail.com>
2015-11-22 00:51:27 +0100

    Set up for long running tests

--- module_5/MnistRunner.py
+++ module_5/MnistRunner.py
@@ -43,6 +43,13 @@ def convert_all_labels_to_unary_lists(label_list, max_value=9):
 
 if __name__ == '__main__':
 
+    topologies = [[100],
+                  [200],
+                  [300],
+                  [100, 100],
+                  [200, 200]]
+
+
     try:
         f_test = open('test_cases.pickle', 'rb')
         f_training = open('training_cases.pickle', 'rb')
@@ -75,34 +82,36 @@ if __name__ == '__main__':
 
     print('creating network')
 
-    hidden_layer_topology = [200, 150, 100]
-    error_function = 'binary_cross'
-    mnist_neural_net = mn(784, hidden_layer_topology, 10, training_cases, test_cases, error_func=error_function)
+    for t in topologies:
+        for i in range(20):
+            hidden_layer_topology = t
+            error_function = 'binary_cross'
+            mnist_neural_net = mn(784, hidden_layer_topology, 10, training_cases, test_cases, error_func=error_function)
 
-    a = time.time()
+            a = time.time()
 
-    no_of_epochs = 20
-    error_rates = mnist_neural_net.do_training(no_of_epochs)
-    print("Error rates:", error_rates)
-    percentage_wrong_on_test_set = mnist_neural_net.get_percentage_of_tests_wrong()
-    print("Error rate on test_set:", mnist_neural_net.get_percentage_of_tests_wrong())
-    percentage_wrong_on_training_set = mnist_neural_net.get_percentage_of_tests_wrong(mnist_neural_net.training_cases)
-    print("Error rate on training_set:", percentage_wrong_on_training_set)
+            no_of_epochs = 20
+            error_rates = mnist_neural_net.do_training(no_of_epochs)
+            print("Error rates:", error_rates)
+            percentage_wrong_on_test_set = mnist_neural_net.get_percentage_of_tests_wrong()
+            print("Error rate on test_set:", mnist_neural_net.get_percentage_of_tests_wrong())
+            percentage_wrong_on_training_set = mnist_neural_net.get_percentage_of_tests_wrong(mnist_neural_net.training_cases)
+            print("Error rate on training_set:", percentage_wrong_on_training_set)
 
-    b = time.time()
+            b = time.time()
 
-    time_elapsed = round(b-a)
-    print('Time elapsed during training and testing:', time_elapsed)
+            time_elapsed = round(b-a)
+            print('Time elapsed during training and testing:', time_elapsed)
 
-    with open("mnist_log.txt", "a") as logfile:
-        timestamp = dt.fromtimestamp(a)
-        readable_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-        logfile.write("\n\nTest started at " + readable_timestamp)
-        logfile.write("\nTime elapsed: " + str(time_elapsed))
-        logfile.write("\nHidden layer topology: " + str(hidden_layer_topology))
-        logfile.write("\nError function: " + error_function)
-        logfile.write("\nNumber of epochs: " + str(no_of_epochs))
-        logfile.write("\nTraining error rates: " + str(list(enumerate(error_rates, 1))))
-        logfile.write("\nPercentage wrong on test cases: " + str(percentage_wrong_on_test_set))
-        logfile.write("\nPercentage wrong on training cases: " + str(percentage_wrong_on_training_set))
+            with open("mnist_log.txt", "a") as logfile:
+                timestamp = dt.fromtimestamp(a)
+                readable_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+                logfile.write("\n\nTest started at " + readable_timestamp)
+                logfile.write("\nTime elapsed: " + str(time_elapsed))
+                logfile.write("\nHidden layer topology: " + str(hidden_layer_topology))
+                logfile.write("\nError function: " + error_function)
+                logfile.write("\nNumber of epochs: " + str(no_of_epochs))
+                logfile.write("\nTraining error rates: " + str(list(enumerate(error_rates, 1))))
+                logfile.write("\nPercentage wrong on test cases: " + str(percentage_wrong_on_test_set))
+                logfile.write("\nPercentage wrong on training cases: " + str(percentage_wrong_on_training_set))
 
