3847883b1fb0c253766f3eedfaab0d502a0e3d90
vages <eirikvageskar@gmail.com>
2015-11-24 00:17:17 +0100

    Refactor 2048-player

--- module_6/Play2048WithNetwork.py
+++ module_6/Play2048WithNetwork.py
@@ -1,13 +1,19 @@
 import random
+import statistics
 import time
 from math import log
 from tkinter import Tk
+import ai2048demo
+
+import datetime
+dt = datetime.datetime
 
 __author__ = 'eirikvageskar'
 from module_4.PowerBoard import PowerBoard
 from PowerBoardAnn import PowerBoardAnn
 from Visuals import GameWindow
 import sys
+
 
 def convert_board_to_exponents(board):
     a = []
@@ -19,51 +25,65 @@ def convert_board_to_exponents(board):
 
     return a
 
-def convert_list_to_2d(board_list):
 
+def convert_list_to_2d(board_list):
     return [board_list[:4], board_list[4:8], board_list[8:12], board_list[12:]]
 
 if __name__ == '__main__':
     directions = ('u', 'r', 'd', 'l')
-    pb = PowerBoard((4, 4))
-    pb.add_random_tile()
 
     hidden_layer_topology = [20, 20]
     error_function = 'binary_cross'
-    pba = PowerBoardAnn(18, hidden_layer_topology, 4)
+    pba = PowerBoardAnn(26, hidden_layer_topology, 4)
     pba.read_pickle()
-    no_of_epochs = 40
+    no_of_epochs = 10
     error_rates = pba.do_training(no_of_epochs)
 
-    #root = Tk()
-    #gw = GameWindow(root)
+    start_time = time.time()
 
-    #gw.update_view(pb.get_board())
-    #root.mainloop()
+    ann_best_tiles = []
+    random_best_tiles = []
+    for i in range(50):
+        pb = PowerBoard((4, 4))
+        pb.add_random_tile()
+        while True:
+            if pb.is_game_over():
+                pb.print_to_console()
+                ann_best_tiles.append(max(pb.get_board()))
+                break
+            network_output = pba.evaluate_one_board(convert_list_to_2d(pb.get_board()))
+            possible_directions = pb.get_possible_move_directions()
+            for choice in network_output:
+                if directions[choice] in possible_directions:
+                    pb.move_and_add_random_tile(directions[choice])
+                    break
 
-    while True:
-        if pb.is_game_over():
-            break
-        network_output = pba.evaluate_one_board(convert_list_to_2d(pb.get_board()))
-        direction = directions[network_output]
-        if direction not in pb.get_possible_move_directions():
-            direction = random.choice(pb.get_possible_move_directions())
-        pb.move_and_add_random_tile(direction)
-        pb.print_to_console()
-        #gw.update_view(convert_board_to_exponents(pb.get_board()))
+    for i in range(50):
+        pb = PowerBoard((4, 4))
+        pb.add_random_tile()
+        while True:
+            if pb.is_game_over():
+                pb.print_to_console()
+                random_best_tiles.append(max(pb.get_board()))
+                break
+            possible_directions = pb.get_possible_move_directions()
+            pb.move_and_add_random_tile(random.choice(possible_directions))
 
-    """
-    while True:
+    print(ann_best_tiles)
+    ann_mean = statistics.mean(ann_best_tiles)
+    print("Mean best tile for ANN:", ann_mean)
+    print(random_best_tiles)
+    random_mean = statistics.mean(random_best_tiles)
+    print("Mean best tile for random:", random_mean)
+    print("Welch test score: ", ai2048demo.welch(random_best_tiles, ann_best_tiles))
 
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    pb.move_and_add_random_tile('d')
-                if event.key == pygame.K_UP:
-                    pb.move_and_add_random_tile('u')
-                if event.key == pygame.K_RIGHT:
-                    pb.move_and_add_random_tile('r')
-                if event.key == pygame.K_LEFT:
-                    pb.move_and_add_random_tile('l')
-
-        gfx.draw()
-    """
+    with open("play2048log.txt", "a") as logfile:
+        timestamp = dt.fromtimestamp(start_time)
+        readable_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        logfile.write("\n\nTest started at " + readable_timestamp)
+        logfile.write("\nHidden layer topology: " + str(hidden_layer_topology))
+        logfile.write("\nNumber of epochs: " + str(no_of_epochs))
+        logfile.write("\nANN best tiles: " + str(ann_best_tiles))
+        logfile.write("\nANN average tile: " + str(ann_mean))
+        logfile.write("\nRandom best tiles: " + str(random_best_tiles))
+        logfile.write("\nRandom average tile: " + str(random_mean))
