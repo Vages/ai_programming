4f70fa040984571ecb8a83c75b1e60cfbf131cea
vages <eirikvageskar@gmail.com>
2015-11-24 00:19:37 +0100

    Remove loop logic, refactor scaling function, take command line arguments

--- module_5/MnistRunner.py
+++ module_5/MnistRunner.py
@@ -1,7 +1,4 @@
 import pickle
-
-__author__ = 'eirikvageskar'
-
 import time
 import basics.mnist_basics as mb
 import MnistNetwork
@@ -11,6 +8,8 @@ import sys
 dt = datetime.datetime
 
 mn = MnistNetwork.MnistNetwork
+
+__author__ = 'eirikvageskar'
 
 
 def apply_division_to_all_lists(list_of_lists, factor=255):
@@ -23,6 +22,8 @@ def apply_division_to_all_lists(list_of_lists, factor=255):
     """
     new_list = []
     for number_list in list_of_lists:
+        factor = max(number_list)
+        print(factor)
         new_list.append([entry/factor for entry in number_list])
 
     return new_list
@@ -41,8 +42,12 @@ def convert_all_labels_to_unary_lists(label_list, max_value=9):
 
 
 if __name__ == '__main__':
+    no_of_epochs = int(sys.argv[1])
+    raw_topologies = sys.argv[2]
+    error_function = sys.argv[3]
+    activation_function = sys.argv[4]
 
-    topologies = [[200, 150, 100]]
+    hidden_layer_topology = [int(raw) for raw in raw_topologies.split(";")]
 
     try:
         f_training_inputs = open('training_inputs.pickle', 'rb')
@@ -88,42 +93,39 @@ if __name__ == '__main__':
 
         print('Saved training and test sets with pickle')
 
-    for t in topologies:
-        for i in range(20):
-            hidden_layer_topology = t
-            error_function = 'binary_cross'
-            mnist_neural_net = mn(784, hidden_layer_topology, 10, training_inputs, training_outputs, test_inputs,
-                                  test_outputs, error_function=error_function)
+    mnist_neural_net = mn(784, hidden_layer_topology, 10, training_inputs, training_outputs, test_inputs,
+                          test_outputs, activation_function=activation_function, error_function=error_function)
 
-            a = time.time()
+    a = time.time()
 
-            no_of_epochs = 20
-            error_rates = mnist_neural_net.do_training(no_of_epochs)
-            print("Error rates:", error_rates)
-            percentage_wrong_on_test_set = mnist_neural_net.get_percentage_of_tests_wrong()
-            print("Error rate on test_set:", mnist_neural_net.get_percentage_of_tests_wrong())
-            percentage_wrong_on_training_set = \
-                mnist_neural_net.get_percentage_of_tests_wrong(mnist_neural_net.training_inputs,
-                                                               mnist_neural_net.training_outputs)
 
-            print("Error rate on training_set:", percentage_wrong_on_training_set)
+    error_rates = mnist_neural_net.do_training(no_of_epochs)
+    print("Error rates:", error_rates)
+    percentage_wrong_on_test_set = mnist_neural_net.get_percentage_of_tests_wrong()
+    print("Error rate on test_set:", mnist_neural_net.get_percentage_of_tests_wrong())
+    percentage_wrong_on_training_set = \
+        mnist_neural_net.get_percentage_of_tests_wrong(mnist_neural_net.training_inputs,
+                                                       mnist_neural_net.training_outputs)
 
-            b = time.time()
+    print("Error rate on training_set:", percentage_wrong_on_training_set)
 
-            time_elapsed = round(b-a)
-            print('Time elapsed during training and testing:', time_elapsed)
+    b = time.time()
 
-            logfile = open("mnist_log.txt", "a")
+    time_elapsed = round(b-a)
+    print('Time elapsed during training and testing:', time_elapsed)
 
-            timestamp = dt.fromtimestamp(a)
-            readable_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
-            logfile.write("\n\nTest started at " + readable_timestamp)
-            logfile.write("\nTime elapsed: " + str(time_elapsed))
-            logfile.write("\nHidden layer topology: " + str(hidden_layer_topology))
-            logfile.write("\nError function: " + error_function)
-            logfile.write("\nNumber of epochs: " + str(no_of_epochs))
-            logfile.write("\nTraining error rates: " + str(list(enumerate(error_rates, 1))))
-            logfile.write("\nPercentage wrong on test cases: " + str(percentage_wrong_on_test_set))
-            logfile.write("\nPercentage wrong on training cases: " + str(percentage_wrong_on_training_set))
+    logfile = open("mnist_log.txt", "a")
 
-            logfile.close()
+    timestamp = dt.fromtimestamp(a)
+    readable_timestamp = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    logfile.write("\n\nTest started at " + readable_timestamp)
+    logfile.write("\nTime elapsed: " + str(time_elapsed))
+    logfile.write("\nHidden layer topology: " + str(hidden_layer_topology))
+    logfile.write("\nError function: " + error_function)
+    logfile.write("\nActivation function: " + activation_function)
+    logfile.write("\nNumber of epochs: " + str(no_of_epochs))
+    logfile.write("\nTraining error rates: " + str(list(enumerate(error_rates, 1))))
+    logfile.write("\nPercentage wrong on test cases: " + str(percentage_wrong_on_test_set))
+    logfile.write("\nPercentage wrong on training cases: " + str(percentage_wrong_on_training_set))
+
+    logfile.close()
