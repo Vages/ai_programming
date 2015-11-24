d5dcf26d4e15bc459e0dc72f3528af05962eae06
vages <eirikvageskar@gmail.com>
2015-11-24 03:27:11 +0100

    Move much of the logic into the MnistNetwork class

--- module_5/MnistNetwork.py
+++ module_5/MnistNetwork.py
@@ -10,13 +10,36 @@ class MnistNetwork(ArtificialNeuralNetwork.ArtificialNeuralNetwork):
     Subclass of Artificial Neural Network class which implements some helper methods useful for the
     Mnist-classification task
     """
+    @staticmethod
+    def normalize_list(input_list):
+        """
+        Normalizes a list of numbers
+        :param input_list:
+        :return:
+        """
+        factor = max(input_list)
+        return [entry/factor for entry in input_list]
+
+    @staticmethod
+    def convert_int_to_unary_list(integer, max_value=9):
+        temp = [0]*(max_value+1)
+
+        temp[integer] = 1
+
+        return temp
 
     def __init__(self, input_nodes_no, hidden_nodes_topology, output_nodes_no, training_inputs=None,
                  training_outputs=None, test_inputs=None, test_outputs=None, learning_rate=0.1,
                  activation_function='sigmoid', error_function='binary_cross'):
 
-        super(MnistNetwork, self).__init__(input_nodes_no, hidden_nodes_topology, output_nodes_no, training_inputs,
-                                           training_outputs, test_inputs, test_outputs, learning_rate,
+        normalized_training_inputs = [MnistNetwork.normalize_list(l) for l in training_inputs]
+        normalized_test_inputs = [MnistNetwork.normalize_list(l) for l in test_inputs]
+
+        vectorized_training_outputs = [MnistNetwork.convert_int_to_unary_list(label) for label in training_outputs]
+        vectorized_test_outputs = [MnistNetwork.convert_int_to_unary_list(label) for label in test_outputs]
+
+        super(MnistNetwork, self).__init__(input_nodes_no, hidden_nodes_topology, output_nodes_no, normalized_training_inputs,
+                                           vectorized_training_outputs, normalized_test_inputs, vectorized_test_outputs, learning_rate,
                                            activation_function, error_function)
 
     def get_percentage_of_tests_wrong(self, test_inputs=None, test_outputs=None):
@@ -46,6 +69,8 @@ class MnistNetwork(ArtificialNeuralNetwork.ArtificialNeuralNetwork):
 
         outputs = []
 
+        feature_sets = [MnistNetwork.normalize_list(fs) for fs in feature_sets]
+
         for input_v in feature_sets:
             outputs.append(self.output_for_input(input_v))
 
@@ -54,7 +79,7 @@ class MnistNetwork(ArtificialNeuralNetwork.ArtificialNeuralNetwork):
         for o in outputs:
             numbers.append(self.find_maximum_list_entry_index(o))
 
-        return outputs
+        return numbers
 
     @staticmethod
     def find_maximum_list_entry_index(number_list):
--- module_5/MnistRunner.py
+++ module_5/MnistRunner.py
@@ -12,35 +12,6 @@ mn = MnistNetwork.MnistNetwork
 __author__ = 'eirikvageskar'
 
 
-def apply_division_to_all_lists(list_of_lists, factor=255):
-    """
-    Downscales a lists of lists by a certain factor
-
-    :param list_of_lists:
-    :param factor:
-    :return:
-    """
-    new_list = []
-    for number_list in list_of_lists:
-        factor = max(number_list)
-        print(factor)
-        new_list.append([entry/factor for entry in number_list])
-
-    return new_list
-
-
-def convert_int_to_unary_list(integer, max_value=9):
-    temp = [0]*(max_value+1)
-
-    temp[integer] = 1
-
-    return temp
-
-
-def convert_all_labels_to_unary_lists(label_list, max_value=9):
-    return [convert_int_to_unary_list(i, max_value) for i in label_list]
-
-
 if __name__ == '__main__':
     no_of_epochs = int(sys.argv[1])
     raw_topologies = sys.argv[2]
@@ -49,57 +20,17 @@ if __name__ == '__main__':
 
     hidden_layer_topology = [int(raw) for raw in raw_topologies.split(";")]
 
-    try:
-        f_training_inputs = open('training_inputs.pickle', 'rb')
-        f_training_outputs = open('training_outputs.pickle', 'rb')
-        f_test_inputs = open('test_inputs.pickle', 'rb')
-        f_test_outputs = open('test_outputs.pickle', 'rb')
-
-        training_inputs = pickle.load(f_training_inputs)
-        training_outputs = pickle.load(f_training_outputs)
-        test_inputs = pickle.load(f_test_inputs)
-        test_outputs = pickle.load(f_test_outputs)
-
-        f_training_inputs.close()
-        f_training_outputs.close()
-        f_test_inputs.close()
-        f_test_outputs.close()
-
-        print('Loaded training and test sets with pickle')
-
-    except FileNotFoundError:
-        training_inputs_raw, training_labels_raw = mb.load_cases('all_flat_mnist_training_cases', nested=False)
-        test_inputs_raw, test_labels_raw = mb.load_cases('all_flat_mnist_testing_cases', nested=False)
-
-        training_inputs = apply_division_to_all_lists(training_inputs_raw)
-        training_outputs = convert_all_labels_to_unary_lists(training_labels_raw)
-        test_inputs = apply_division_to_all_lists(test_inputs_raw)
-        test_outputs = convert_all_labels_to_unary_lists(test_labels_raw)
-
-        f_test_inputs = open('test_inputs.pickle', 'wb')
-        f_test_outputs = open('test_outputs.pickle', 'wb')
-        f_training_inputs = open('training_inputs.pickle', 'wb')
-        f_training_outputs = open('training_outputs.pickle', 'wb')
-
-        pickle.dump(test_inputs, f_test_inputs)
-        pickle.dump(test_outputs, f_test_outputs)
-        pickle.dump(training_inputs, f_training_inputs)
-        pickle.dump(training_outputs, f_training_outputs)
-
-        f_test_inputs.close()
-        f_test_outputs.close()
-        f_training_inputs.close()
-        f_training_outputs.close()
-
-        print('Saved training and test sets with pickle')
+    training_inputs, training_outputs = mb.load_cases('all_flat_mnist_training_cases', nested=False)
+    test_inputs, test_outputs = mb.load_cases('all_flat_mnist_testing_cases', nested=False)
 
     mnist_neural_net = mn(784, hidden_layer_topology, 10, training_inputs, training_outputs, test_inputs,
                           test_outputs, activation_function=activation_function, error_function=error_function)
 
+    print("created net")
     a = time.time()
 
-
     error_rates = mnist_neural_net.do_training(no_of_epochs)
+    print(mnist_neural_net.blind_test(mb.load_cases('demo100', nested=False)[0]))
     print("Error rates:", error_rates)
     percentage_wrong_on_test_set = mnist_neural_net.get_percentage_of_tests_wrong()
     print("Error rate on test_set:", mnist_neural_net.get_percentage_of_tests_wrong())
@@ -107,7 +38,9 @@ if __name__ == '__main__':
         mnist_neural_net.get_percentage_of_tests_wrong(mnist_neural_net.training_inputs,
                                                        mnist_neural_net.training_outputs)
 
+    mb.minor_demo(mnist_neural_net)
     print("Error rate on training_set:", percentage_wrong_on_training_set)
+
 
     b = time.time()
 
